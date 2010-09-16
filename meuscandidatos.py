c87447a62a0c6f4482a687ec22458ec77e45cd1e
Gustavo Franco <stratus@acm.org>
2010-09-15 21:59:51 -0300

    Entrada de candidatos e `view' ok. . Me certifiquei que ao submeter o form, o usuário cai em seu `permalink' com os candidatos inseridos. Removi bastante código redundante.

--- meuscandidatos.py
+++ meuscandidatos.py
@@ -1,62 +1,72 @@
-import cgi
+#!/usr/bin/env python
+#
+# Copyright 2010 Gustavo Franco <stratus@acm.org>
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing sharepermissions and
+# limitations under the License.
 
-#from google.appengine.api import users
+"""meuscandidatos."""
+
+__author__ = "stratus@acm.org (Gustavo Franco)"
+
+import os
 from google.appengine.ext import webapp
 from google.appengine.ext.webapp.util import run_wsgi_app
 from google.appengine.ext import db
+from google.appengine.ext.webapp import template
+
+LOOKUP_BY_KEY = "SELECT * FROM MeusCandidatos WHERE __key__ = KEY ('MeusCandidatos', :1)"
+
 
 class MeusCandidatos(db.Model):
-    deputado_estadual = db.IntegerProperty()
-    deputado_federal = db.IntegerProperty()
-    senador_primeiro = db.IntegerProperty()
-    senador_segundo = db.IntegerProperty()
-    governador = db.IntegerProperty()
-    presidente = db.IntegerProperty()
-    date = db.DateTimeProperty(auto_now_add=True)
+  deputado_estadual = db.IntegerProperty()
+  deputado_federal = db.IntegerProperty()
+  senador_primeiro = db.IntegerProperty()
+  senador_segundo = db.IntegerProperty()
+  governador = db.IntegerProperty()
+  presidente = db.IntegerProperty()
+  date = db.DateTimeProperty(auto_now_add=True)
+
 
 class MainPage(webapp.RequestHandler):
-    def get(self):
-        self.response.out.write('<html><body>')
-
-        candidatos = db.GqlQuery("SELECT * FROM MeusCandidatos ORDER BY date DESC LIMIT 10")
-
-        self.response.out.write('Ultimos presidentes registrados')
-        for candidato in candidatos:
-           self.response.out.write('<b>%s</b> para presidente.' % candidato.presidente)
-
-        self.response.out.write("""
-              <form action="/registrar" method="post">
-                <div><input type="text" name="deputado_estadual"></input></div>
-                <div><input type="text" name="deputado_federal"></input></div>
-                <div><input type="text" name="senador_primeiro"></input></div>
-                <div><input type="text" name="senador_segundo"></input></div>
-                <div><input type="text" name="governador"></input></div>
-                <div><input type="text" name="presidente"></input></div>
-                <div><input type="submit" value="Enviar"></div>
-              </form>
-            </body>
-          </html>""")
-
-class Candidatos(webapp.RequestHandler):
-    def post(self):
-        meuscandidatos = MeusCandidatos()
-        meuscandidatos.deputado_estadual = int(self.request.get('deputado_estadual'))
-        meuscandidatos.deputado_federal = int(self.request.get('deputado_federal'))
-        meuscandidatos.senador_primeiro = int(self.request.get('senador_primeiro'))
-        meuscandidatos.senador_segundo = int(self.request.get('senador_segundo'))
-        meuscandidatos.governador = int(self.request.get('governador'))
-        meuscandidatos.presidente = int(self.request.get('presidente'))
-        meuscandidatos.put()
-        self.redirect('/')
+   def get(self, id=None):
+     if id:
+       meuscandidatos = db.GqlQuery(LOOKUP_BY_KEY, int(id))
+       template_values = { "meuscandidatos": meuscandidatos }
+       path = os.path.join(os.path.dirname(__file__), "meuscandidatos.html")
+       self.response.out.write(template.render(path, template_values))
+     else:
+       path = os.path.join(os.path.dirname(__file__), "index.html")
+       self.response.out.write(template.render(path, None))
+       
+   def post(self, unused_id):
+     meuscandidatos = MeusCandidatos()
+     meuscandidatos.deputado_estadual = int(self.request.get("deputado_estadual"))
+     meuscandidatos.deputado_federal = int(self.request.get("deputado_federal"))
+     meuscandidatos.senador_primeiro = int(self.request.get("senador_primeiro"))
+     meuscandidatos.senador_segundo = int(self.request.get("senador_segundo"))
+     meuscandidatos.governador = int(self.request.get("governador"))
+     meuscandidatos.presidente = int(self.request.get("presidente"))
+     key = meuscandidatos.put()
+     self.redirect('/' + str(key.id()))
 
 
-application = webapp.WSGIApplication(
-                                     [('/', MainPage),
-                                      ('/registrar', Candidatos)],
+application = webapp.WSGIApplication([(r"/(.*)", MainPage)],
                                      debug=True)
 
+
 def main():
-    run_wsgi_app(application)
+   run_wsgi_app(application)
+
 
 if __name__ == "__main__":
-    main()
+   main()
